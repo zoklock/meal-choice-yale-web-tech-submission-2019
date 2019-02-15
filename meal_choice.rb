c17f9903543ac249468c71b6a883a5b3e093a714
Michelle Minzhi Li <michelle.li.mml59@yale.edu>
2019-02-15 20:35:12 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,6 +1,6 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
 def meal_choice(meal="meat")
-  gets "meal"
+  meal = gets
   puts "meal"
 end
